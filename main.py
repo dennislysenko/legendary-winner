787fa78c0211008bed08c678f73722b460f8d924
Dennis Lysenko <dennis.s.lysenko@gmail.com>
2017-01-14 14:30:50 -0500

    reorganize to facilitate editing modules separately

--- main.py
+++ main.py
@@ -1,50 +1,68 @@
 import json
 import random
 
-emojis_data = open('emojis.json').read()
-emojis = json.loads(emojis_data)
+# ==============================================================================
+# ================================== MODULES ===================================
+# ==============================================================================
 
-# schema:
-# "emojikey": { "keywords": [...], "char": <emoji>, "category": "people" }
+def cleanup_word(word):
+	# TODO write
+	return word
 
-keyword_emoji_map = dict()
-for (name, entry) in emojis.iteritems():
-	emoji = entry['char']
-	keywords = entry['keywords']
-	for keyword in keywords:
-		if keyword not in keyword_emoji_map:
-			keyword_emoji_map[keyword] = []
+class EmojiMap:
+	keyword_emoji_map = dict()
 
-		if emoji not in keyword_emoji_map[keyword] and emoji is not None:
-			keyword_emoji_map[keyword].append(emoji)
+	def __init__(self):
+		self.construct_emoji_map()
 
-def encode_emoji(emoji):
-	return emoji.encode('UTF-8')
+	def construct_emoji_map(self):
+		emojis_data = open('emojis.json').read()
+		emojis = json.loads(emojis_data)
 
-# proof of concept, make sure our map is correct
-f = open('test.txt', 'w')
-for keyword in keyword_emoji_map.keys()[0:5]:
-	for emoji in keyword_emoji_map[keyword]:
-		f.write(emoji.encode('UTF-8'))
-f.close()
+		# schema:
+		# "emojikey": { "keywords": [...], "char": <emoji>, "category": "people" }
+
+		for (name, entry) in emojis.iteritems():
+			emoji = entry['char']
+			keywords = entry['keywords']
+			for keyword in keywords:
+				if keyword not in self.keyword_emoji_map:
+					self.keyword_emoji_map[keyword] = []
+
+				if emoji not in self.keyword_emoji_map[keyword] and emoji is not None:
+					self.keyword_emoji_map[keyword].append(emoji)
+
+	def get_emoji(self, cleaned_word):
+		if cleaned_word in self.keyword_emoji_map and len(self.keyword_emoji_map[cleaned_word]) > 0:
+			emojis = self.keyword_emoji_map[cleaned_word]
+			
+			randIndex = random.sample(range(len(emojis)), 1)
+			randIndex.sort()
+
+			emoji = emojis[randIndex[0]]
+
+			return emoji
+
+# TODO maybe implement emoji frequency module?
+
+# ==============================================================================
+# ================================== DRIVER ===================================
+# ==============================================================================
+
+emoji_map = EmojiMap()
 
 # process copypasta
 copypasta = open('input.txt').read()
 f = open('output.txt', 'w')
+
 for word in copypasta.split():
-	# TODO clean up word
-	cleaned_word = word
+	cleaned_word = cleanup_word(word)
 
 	f.write(word.encode('UTF-8'))
 
-	if cleaned_word in keyword_emoji_map and len(keyword_emoji_map[cleaned_word]) > 0:
-		emojis = keyword_emoji_map[cleaned_word]
-		
-		randIndex = random.sample(range(len(emojis)), 1)
-		randIndex.sort()
+	emoji = emoji_map.get_emoji(cleaned_word)
 
-		emoji = emojis[randIndex[0]]
-		
+	if emoji is not None:
 		f.write(' ' + emoji.encode('UTF-8') + ' ')
 
 	f.write(' ')
